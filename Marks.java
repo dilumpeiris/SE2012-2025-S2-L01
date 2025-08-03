ab3cf6b9ee2fa01f84c024e83ba01cd66faf103f
dilumpeiris <dilum.peiris@icloud.com>
2025-08-03 19:51:49 +0530

    finished question 2 and 3

--- Marks.java
+++ Marks.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class ClassRoom{
+public class Marks{
 	// All the student information (their marks for each subject) is stored in a format of (studentID - 1) * 3 + (subjectID - 1).
 	int[] studentMarks;
 	// Total students amount.
@@ -8,15 +8,15 @@ public class ClassRoom{
 
 public static void main(String[] args) {
 		Scanner scanner = new Scanner(System.in);
-		ClassRoom cr = new ClassRoom();
+		Marks mo = new Marks();
 
 		System.out.print("Enter the number of students in the classroom: ");
-		cr.n = scanner.nextInt();
+		mo.n = scanner.nextInt();
 
 		// To catch the empty line after nextInt().
 		scanner.nextLine();
 
-		cr.studentMarks = new int[cr.n * 3];
+		mo.studentMarks = new int[mo.n * 3];
 
 		System.out.println("1. Add student marks               : add [studentID]- student ID will be an integer ranging from 1 to n");
 		System.out.println("2. Update student mark             : update [studentID] [subjectID] - subject ID will be an integer from 1 to 3");
@@ -39,38 +39,38 @@ public static void main(String[] args) {
 				if(action.equals("add")) {
 					int studentID = Integer.parseInt(parts[1]);
 
-					addStudentMark(studentID, scanner, cr);
+					addStudentMark(studentID, scanner, mo);
 				}
 				else if(action.equals("update")) {
 					int studentID = Integer.parseInt(parts[1]);
 					int subjectID = Integer.parseInt(parts[2]);
 
-					updateMark(studentID, subjectID, scanner, cr);
+					updateMark(studentID, subjectID, scanner, mo);
 				}
 				else if(action.equals("average_s")) {
 					int subjectID = Integer.parseInt(parts[1]);
 
-					average_s(subjectID, cr);
+					average_s(subjectID, mo);
 				}
 				else if(action.equals("average")) {
 					int studentID = Integer.parseInt(parts[1]);
 
-					average(studentID, cr);
+					average(studentID, mo);
 				}
 				else if(action.equals("total")) {
 					int studentID = Integer.parseInt(parts[1]);
 
-					total(studentID, cr);
+					total(studentID, mo);
 				}
 				else if(action.equals("grades")) {
 					int studentID = Integer.parseInt(parts[1]);
-					grades(studentID, cr);
+					grades(studentID, mo);
 				}
 				else if(action.equals("quit")) {
 					break;
 				}
 				else {
-					System.out.println("Invalid Command");
+					System.out.println("!> Invalid Command");
 				}
 				} catch (ArrayIndexOutOfBoundsException e) {
 					System.out.println("!> Missing Arguments");
@@ -81,46 +81,46 @@ public static void main(String[] args) {
 		scanner.close();
 	}
 // Add student marks in bulk.
-public static void addStudentMark(int studentID, Scanner scanner, ClassRoom cr){
-		if(studentID > cr.n) {
+public static void addStudentMark(int studentID, Scanner scanner, Marks mo){
+		if(studentID > mo.n) {
 			System.out.println("!> Invalid Index");
 
 			return;
 		}
 		System.out.print("$ Math: ");
-		cr.studentMarks[(studentID - 1) * 3 + 0]  = scanner.nextInt();
+		mo.studentMarks[(studentID - 1) * 3 + 0]  = scanner.nextInt();
 
 		// To catch the empty line after nextInt().
 		scanner.nextLine();
 
 		System.out.print("$ Chemistry: ");
-		cr.studentMarks[(studentID - 1) * 3 + 1]  = scanner.nextInt();
+		mo.studentMarks[(studentID - 1) * 3 + 1]  = scanner.nextInt();
 
 		// To catch the empty line after nextInt().
 		scanner.nextLine();
 
 		System.out.print("$ Physics: ");
-		cr.studentMarks[(studentID - 1) * 3 + 2]  = scanner.nextInt();
+		mo.studentMarks[(studentID - 1) * 3 + 2]  = scanner.nextInt();
 
 		// To catch the empty line after nextInt().
 		scanner.nextLine();
 	}
-public static void updateMark(int studentID, int subjectID, Scanner scanner, ClassRoom cr) {
-		if(studentID > cr.n || subjectID > 3) {
+public static void updateMark(int studentID, int subjectID, Scanner scanner, Marks mo) {
+		if(studentID > mo.n || subjectID > 3) {
 			System.out.println("!> Invalid Index");
 
 			return;
 		}
 
 		System.out.print("$ Mark: ");
-		cr.studentMarks[(studentID - 1) * 3 + (subjectID - 1)]  = scanner.nextInt();
+		mo.studentMarks[(studentID - 1) * 3 + (subjectID - 1)]  = scanner.nextInt();
 
 		// To catch the empty line after nextInt().
 		scanner.nextLine();
 
 	}
-public static void average(int studentID, ClassRoom cr) {
-		if(studentID > cr.n) {
+public static void average(int studentID, Marks mo) {
+		if(studentID > mo.n) {
 			System.out.println("!> Invalid Index");
 
 			return;
@@ -129,12 +129,12 @@ public static void average(int studentID, ClassRoom cr) {
 		int mark = 0;
 
 		for(int i = (studentID - 1) * 3; i < ((studentID - 1) * 3 + 3); i++) {
-			mark += cr.studentMarks[i];
+			mark += mo.studentMarks[i];
 		}
 
 		System.out.println(">> Average Student Mark: " + (float)mark/3);
 	}
-public static void average_s(int subjectID, ClassRoom cr) {
+public static void average_s(int subjectID, Marks mo) {
 		if(subjectID > 3) {
 			System.out.println("!> Invalid Index");
 
@@ -144,15 +144,15 @@ public static void average_s(int subjectID, ClassRoom cr) {
 		int mark = 0;
 
 		// Stepping by 3 to access subjects.
-		for(int i = 0; i < cr.n*3; i += 3) {
+		for(int i = 0; i < mo.n*3; i += 3) {
 			// Subject = i + (subjectID - 1).
-			mark += cr.studentMarks[i + (subjectID - 1)];
+			mark += mo.studentMarks[i + (subjectID - 1)];
 		}
 
-		System.out.println(">> Average Subject Mark: " + (float)mark/cr.n);
+		System.out.println(">> Average Subject Mark: " + (float)mark/mo.n);
 	}
-public static void total(int studentID, ClassRoom cr) {
-		if(studentID > cr.n) {
+public static void total(int studentID, Marks mo) {
+		if(studentID > mo.n) {
 			System.out.println("!> Invalid Index");
 
 			return;
@@ -162,13 +162,13 @@ public static void total(int studentID, ClassRoom cr) {
 
 		// from i = where the student's subjects starts in studentMarks to + 3 where they end.
 		for(int i = (studentID - 1) * 3; i < ((studentID - 1) * 3 + 3); i++) {
-			mark += cr.studentMarks[i];
+			mark += mo.studentMarks[i];
 		}
 
 		System.out.println(">> Total Mark: " + mark);
 	}
-public static void grades(int studentID, ClassRoom cr) {
-		if(studentID > cr.n) {
+public static void grades(int studentID, Marks mo) {
+		if(studentID > mo.n) {
 			System.out.println("!> Invalid Index");
 
 			return;
@@ -181,7 +181,7 @@ public static void grades(int studentID, ClassRoom cr) {
 			// Have to reset j to 0 to use with grades.
 			int j = i - (studentID - 1) * 3;
 
-			int mark = cr.studentMarks[i];
+			int mark = mo.studentMarks[i];
 
 			grades[j] = "Fail";
 			if(mark >= 90) {grades[j] = "Grade A";}
